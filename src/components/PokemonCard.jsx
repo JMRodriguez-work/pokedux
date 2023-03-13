7db07c80ea291a4b32b50ad987bdb75b1d44903c
Juan Martin Rodriguez <jmartinrodriguez.work@gmail.com>
2023-03-13 14:05:46 -0300

    ADD: React toolkit, not finished

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,16 +1,29 @@
 /* eslint-disable camelcase */
 import React from 'react'
-import { MdFavoriteBorder } from 'react-icons/md'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { useDispatch } from 'react-redux'
+import { setFavorite } from '../actions'
 
-const PokemonCard = ({ name, image, types }) => {
+const PokemonCard = ({ name, image, types, isFavorite, id }) => {
+  const dispatch = useDispatch()
   const pokemonTypes = types.map(item => item.type.name)
+  const handleFavorite = () => {
+    dispatch(setFavorite({ pokemonId: id }))
+  }
 
   return (
     <div className='relative flex flex-col items-center outline-current group bg-gray-400 text-gray-800 dark:text-slate-200 font-semibold text-xl w-fit p-3 rounded-md hover:scale-110 hover:outline ease-out duration-100 cursor-pointer'>
-      <MdFavoriteBorder
-        size={25}
-        className='absolute top-2 right-2 fill-red-700 '
-      />
+      {isFavorite
+        ? <MdFavorite
+            size={25}
+            className=' absolute top-2 right-2 fill-red-600'
+            onClick={handleFavorite}
+          />
+        : <MdFavoriteBorder
+            size={25}
+            className='absolute top-2 right-2 fill-red-600 '
+            onClick={handleFavorite}
+          />}
       <div className='bg-gray-200 min-h-[150px] w-1/2 max-w-[200px] min-w-[150px]  rounded-full border-current transition group-hover:outline'>
         <img
           src={image}
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -3,11 +3,13 @@ import { PokemonCard } from './PokemonCard'
 
 const PokemonList = ({ pokemons }) => {
   return (
-    <div className='mx-4 mt-2 flex max-w-[1700px] flex-wrap items-center justify-center gap-10 overflow-hidden py-5 ease duration-500 '>
-      {pokemons.map((pokemon) => (
-        <PokemonCard key={pokemon.name} name={pokemon.name} image={pokemon.sprites.other['official-artwork'].front_default} types={pokemon.types} />
-      ))}
-    </div>
+    <main>
+      <div className='mx-4 mt-2 flex max-w-[1700px] flex-wrap items-center justify-center gap-10 overflow-hidden py-5 ease duration-500 '>
+        {pokemons.map((pokemon) => (
+          <PokemonCard key={pokemon.name} name={pokemon.name} image={pokemon.sprites.other['official-artwork'].front_default} types={pokemon.types} id={pokemon.id} isFavorite={pokemon.favorite} />
+        ))}
+      </div>
+    </main>
   )
 }
 
